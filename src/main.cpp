681207cb1fa75a392407668b8c614de0cca5a895
Bert Ciccone <21688853+bertciccone@users.noreply.github.com>
2017-12-05 20:41:14 -0800

    debug: added NodehError.txt compiler error log

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-#include <math.h>
-#include <uWS/uWS.h>
-#include <chrono>
-#include <iostream>
-#include <thread>
-#include <vector>
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "MPC.h"
 #include "json.hpp"
+#include <chrono>
+#include <iostream>
+#include <math.h>
+#include <thread>
+#include <uWS/uWS.h>
+#include <vector>
 
 // for convenience
 using json = nlohmann::json;
@@ -93,40 +93,98 @@ int main() {
           double v = j[1]["speed"];
 
           /*
-          * TODO: Calculate steering angle and throttle using MPC.
-          *
-          * Both are in between [-1, 1].
-          *
-          */
-          double steer_value;
-          double throttle_value;
+           * TODO: Calculate steering angle and throttle using MPC.
+           *
+           * Both are in between [-1, 1].
+           *
+           */
 
-          json msgJson;
-          // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
-          // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = throttle_value;
+          for (int i = 0; i < ptsx.size(); ++i) {
+            // shift car reference angle to 90 degrees
+            double shift_x = ptsx[i] - px;
+            double shift_y = ptsy[i] - py;
+            ptsx[i] = (shift_x * cos(0 - psi) - shift_y * sin(0 - psi));
+            ptsy[i] = (shift_x * sin(0 - psi) - shift_y * cos(0 - psi));
+          }
 
-          //Display the MPC predicted trajectory 
-          vector<double> mpc_x_vals;
-          vector<double> mpc_y_vals;
+          double *ptrx = &ptsx[0];
+          Eigen::Map<Eigen::VectorXd> ptsx_transform(ptrx, 6);
 
-          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
-          // the points in the simulator are connected by a Green line
+          double *ptry = &ptsy[0];
+          Eigen::Map<Eigen::VectorXd> ptsy_transform(ptry, 6);
 
-          msgJson["mpc_x"] = mpc_x_vals;
-          msgJson["mpc_y"] = mpc_y_vals;
+          auto coeffs = polyfit(ptsx_transform, ptsy_transform, 3);
 
-          //Display the waypoints/reference line
+          // calculate cte and epsi
+          // this is not the exact cte, shortest distance to target path
+          // instead, this is the horizontal distance to the target path
+          double cte = polyeval(coeffs, 0); // cross track error (position)
+          // double epsi = psi - atan(coeffs[1] + 2 * px * coeffs[2] + 3 *
+          // coeffs[3] * pow(px, 2));
+          // this simplification of epsi works because we set psi and px to 0
+          double epsi = -atan(coeffs[1]); // error in psi (steering angle)
+
+          // TODO: account for delay
+          // these values are from Unity for current steering and throttle
+          // predict the future position of the car at time t+dt
+          // acceleration is not readily available so use throttle
+          double steer_value = j[1]["steering_angle"];
+          double throttle_value = j[1]["throttle"];
+
+          Eigen::VectorXd state(6);
+          // x, y, psi, v, cte, epsi
+          state << 0, 0, 0, v, cte, epsi;
+
+          auto vars = mpc.Solve(state, coeffs);
+
+          // Display the waypoints/reference line
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
+          //.. add (x,y) points to list here, points are in reference to the
+          // vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
 
+          double poly_inc = 2.5;
+          int num_points = 25;
+          for (int i = 1; i < num_points; ++i) {
+            next_x_vals.push_back(poly_inc * i);
+            next_y_vals.push_back(polyeval(coeffs, poly_inc * i));
+          }
+
+          // Display the MPC predicted trajectory
+          vector<double> mpc_x_vals;
+          vector<double> mpc_y_vals;
+
+          //.. add (x,y) points to list here, points are in reference to the
+          // vehicle's coordinate system
+          // the points in the simulator are connected by a Green line
+
+          for (int i = 2; i < vars.size(); ++i) {
+            if (i % 2 == 0) {
+              mpc_x_vals.push_back(vars[i]);
+            } else {
+              mpc_y_vals.push_back(vars[i]);
+            }
+          }
+
+          double Lf = 2.67;
+          json msgJson;
+          // these values drive the car
+          // NOTE: Remember to divide by deg2rad(25) before you send the
+          // steering value back. Otherwise the values will be in between
+          // [-deg2rad(25), deg2rad(25] instead of [-1, 1].
+          // From TIPS AND TRICKS: reverse sign of steering for Unity simulator
+          msgJson["steering_angle"] = -vars[0] / (deg2rad(25) * Lf);
+          msgJson["throttle"] = vars[1];
+
+          // draw the yellow line, the target trajectory
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
+          // draw the green line, the calculated trajectory
+          msgJson["mpc_x"] = mpc_x_vals;
+          msgJson["mpc_y"] = mpc_y_vals;
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
